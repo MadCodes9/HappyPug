107c1f67d36ab8e972a19fe09a1a351716d3c9a0
Ceiara Madrigal <90419840+MadCodes9@users.noreply.github.com>
2022-06-17 00:14:00 -0700

    Expanded menu works for Ingredients

--- lib/search_results.dart
+++ lib/search_results.dart
@@ -16,121 +16,171 @@ class MySearchResultsPage extends StatefulWidget {
 }
 
 class _MySearchResultsState extends State<MySearchResultsPage> {
-  String foundLength = "";
-  
+  Map<String, List<String>> results = {};
+  List<String> keys = [];
+  bool _isVisible = true;
+
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () async{
-          Navigator.pop(context);
-          return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(5),
-                alignment: Alignment.centerLeft,
-                child: Image(
-                  height: 200,
-                  width: 200,
-                  image: NetworkImage('https://i.pinimg.com/originals/53/cb/23/53cb231f4c04ae30a04a6e292eb2a48c.jpg'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(5),
+              alignment: Alignment.centerLeft,
+              child: Image(
+                height: 200,
+                width: 200,
+                image: NetworkImage('https://i.pinimg.com/originals/53/cb/23/53cb231f4c04ae30a04a6e292eb2a48c.jpg'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.check_rounded,
+                  color: Colors.lightGreen,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.check_rounded,
-                    color: Colors.lightGreen,
+                Expanded(
+                  child: Text(
+                    "${widget.numOfgreenIngred} Healthy",
+                    style: TextStyle(fontSize: 15),
                   ),
-                  Expanded(
-                    child: Text(
-                      "${widget.numOfgreenIngred} Healthy",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    flex: 0,
+                  flex: 0,
 
-                  ),
-                  Icon(
-                    Icons.warning_amber_rounded,
-                    color: Colors.yellow,
-                  ),
-                  Expanded(
-                    child: Text(
-                      "${widget.numOfyellowIngred} Caution",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    flex: 0,
-
-                  ),
-                  Icon(
-                    Icons.close_rounded,
-                    color: Colors.red,
-                  ),
-                  Expanded(
-                    child: Text(
-                      //"${widget.text.values.elementAt(0).elementAt(1)} Bad",
-                      "${widget.numOfredIngred} Unhealthy",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    //flex: 0,
-                  ),
-                ],
-              ),
-
-              Row(
-               // mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ButtonTheme(
-                    child: MaterialButton(
-                      onPressed: (){
-
-                      },
-                      child: Text("Overall"),
-                    ),
-                  ),
-                  ButtonTheme(
-                    child: MaterialButton(
-                      onPressed: (){
-
-                      },
-                      child: Text("Ingredients"),
-                    ),
-                  ),
-
-                ],
-              ),
-
-              Container(
-                child: Text(
-                  "Ingredients Found: ${widget.foundIngred.keys}", //From home screen
-                  style: TextStyle(fontSize: 20),
                 ),
+                Icon(
+                  Icons.warning_amber_rounded,
+                  color: Colors.yellow,
+                ),
+                Expanded(
+                  child: Text(
+                    "${widget.numOfyellowIngred} Caution",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  flex: 0,
+
+                ),
+                Icon(
+                  Icons.close_rounded,
+                  color: Colors.red,
+                ),
+                Expanded(
+                  child: Text(
+                    //"${widget.text.values.elementAt(0).elementAt(1)} Bad",
+                    "${widget.numOfredIngred} Unhealthy",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  //flex: 0,
+                ),
+              ],
+            ),
+
+            Row(
+              children: [
+                ButtonTheme(
+                  child: TextButton.icon(
+                    label: Text("Overall", style: TextStyle(color: Colors.black)),
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.grey,
+                    ),
+                    onPressed: (){
+
+                    },
+                  ),
+                ),
+
+
+                ButtonTheme(
+                  child: TextButton.icon(
+                    label: Text("Ingredients", style: TextStyle(color: Colors.black)),
+                    icon: Icon(
+                      Icons.arrow_drop_down,
+                      color: Colors.grey,
+                    ),
+                    onPressed: (){
+                      dynamicButtons();
+                      showText();
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            Visibility(
+              visible: _isVisible,
+              child: Column(
+                  children:
+                  keys.map((String data) => TextButton.icon(
+                    onPressed: (){print(data);},
+                    label: Text(data),
+                    icon: Icon(
+                        Icons.arrow_drop_down_circle_rounded,
+                        color: Colors.grey
+                    ),
+                  )
+                  ).toList()
               ),
-              ElevatedButton(
-                  onPressed: (){  //load data from firestore database
-                    FirebaseFirestore.instance.collection("ingredients").get()
-                        .then((querySnapshot) {
-                      print("Successfully load all ingredients");
-                      //print querySnapshot
-                      querySnapshot.docs.forEach((element) {
-                        //print(element.data());
-                        print(element.data()['name']);
-                      });
-                    }).catchError((error){
-                      print("Fail to load all ingredients");
-                      print(error);
+            ),
+
+
+            // Container(
+            //   child:
+            //   pressed1 ? null:null,
+            // ),
+            // Container(
+            //   child: Column(
+            //     children: [
+            //       //Text("Ingredients Found: ${widget.foundIngred.keys}")
+            //       pressed2 ? Column(
+            //        children:
+            //         keys.map((String data) => TextButton.icon(
+            //             onPressed: (){print(data);},
+            //             label: Text(data),
+            //             icon: Icon(
+            //               Icons.arrow_drop_down_circle_rounded,
+            //               color: Colors.grey,
+            //             ),
+            //         )).toList(),):SizedBox()
+            //     ]
+            //   ),
+            // ),
+
+            ElevatedButton(
+                onPressed: (){  //load data from firestore database
+                  FirebaseFirestore.instance.collection("ingredients").get()
+                      .then((querySnapshot) {
+                    print("Successfully load all ingredients");
+                    //print querySnapshot
+                    querySnapshot.docs.forEach((element) {
+                      //print(element.data());
+                      print(element.data()['name']);
                     });
-                  },
-                  child: Text("List all ingredients")
-              )
-            ]
-        ),
-      )
+                  }).catchError((error){
+                    print("Fail to load all ingredients");
+                    print(error);
+                  });
+                },
+                child: Text("List all ingredients")
+            )
+          ]
+       ),
     );
   }
+
+  void dynamicButtons() {
+      results = widget.foundIngred; //can access results
+      keys = widget.foundIngred.keys.toList();
+    }
+
+  void showText(){
+    setState((){
+      _isVisible = !_isVisible;
+    });
+  }
 }
+
